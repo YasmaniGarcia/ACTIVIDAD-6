64230a9672274a62be2e83d360510ad8b3a2070f
Yasmani-Garcia <148841264+YasmaniGarcia@users.noreply.github.com>
2023-12-24 21:28:28 -0500

    Add files via upload

---
+++ Parte_2_Phishing/src/parte_2_phishing/Parte_2_Phishing.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parte_2_phishing;
+
+
+public class Parte_2_Phishing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       // Definir la lista de palabras clave y sus respectivos puntajes
+        String[] keywords = {
+                "banco", "contraseña", "tarjeta de crédito", "seguro social", "subasta",
+                "red social", "pago en línea", "Usuario", "Acceso", "Verificación", "Actualización",
+                "Seguridad", "Confirmación", "Suspensión", "Verificar", "Restablecer", "Caducidad", 
+                "Premio web", "Premio red social", "Gana dinero rápido", "Oferta especial", 
+                "Oportunidad única", "Problema de seguridad", "Inicio de sesión", "Actividad sospechosa",
+                "Ganador del sorteo", "Descuento exclusivo", "Premio de lotería", "Actualización de seguridad",
+                "Información importante"        
+                // Agrega aquí más palabras clave según tus investigaciones
+        };
+        int[] scores = {1, 1, 1, 1, 1, 1, 1, 2, 3, 2, 3, 3, 3, 2, 2, 1, 1, 3, 2, 1, 2, 2, 3, 3, 2, 1, 2, 3, 1, 2};
+
+    }
+    
+}
